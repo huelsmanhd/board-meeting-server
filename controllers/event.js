d99b45957762beb72d0d10dd325a941bc32423d5
Nate Deisler <ineanderthals@Nates-MacBook-Pro.local>
2019-03-15 16:31:53 -0400

    trying

--- controllers/event.js
+++ controllers/event.js
@@ -61,9 +61,10 @@ router.delete("/delete/:id", validateSession, (req, res) => {
     // let userid = req.user.id;
 
     Event.destroy({where: { id: id}})
-    .then(event => res.send(200))
-    .catch(err => res.send(500, err.message))
-})
+    .then(event => res.send(200), () => {
+        res.send(500, err.message)
+}
+)})
 
 
 module.exports = router;
