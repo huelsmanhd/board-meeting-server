23a8d43a3fdbad2fb253364a7d0c83eb02c6b7fa
Nate Deisler <ineanderthals@Nates-MacBook-Pro.local>
2019-03-15 16:13:27 -0400

    heroku sucks

--- controllers/event.js
+++ controllers/event.js
@@ -61,8 +61,8 @@ router.delete("/delete/:id", validateSession, (req, res) => {
     // let userid = req.user.id;
 
     Event.destroy({where: { id: id}})
-    // .then(event => res.send("You removed event" + id))
-    // .catch(err => res.send(500, err.message))
+    .then(event => res.send("You removed event" + id))
+    .catch(err => res.send(500, err.message))
 })
 
 
